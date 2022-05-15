17ba76ae734b87c78b6cae788808c103f521094a
xArthurDev <arthurrguimaraes12@gmail.com>
2022-05-15 19:42:52 -0300

    feat: create authentication guards

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { UserModel } from '../user/user.model';
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string): Promise<UserModel> {
+    const user = await this.authService.validateUser(email, password);
+    return user;
+  }
+}
