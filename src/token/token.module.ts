91c71fc62b86792ac91975a6d39b11c8b410da1b
xArthurDev <arthurrguimaraes12@gmail.com>
2022-05-23 22:52:56 -0300

    fix: nest circular dependency error

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,11 +1,11 @@
 import { UsersModule } from './../user/user.module';
 import { AuthService } from './../auth/auth.service';
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [JwtModule.register({}), forwardRef(() => UsersModule)],
+  imports: [JwtModule.register({}), UsersModule],
   controllers: [],
   providers: [TokenService, AuthService],
   exports: [TokenService],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,10 @@ import { TokenModule } from 'src/token/token.module';
 /* eslint-disable prettier/prettier */
 import { UsersController } from './user.controller';
 import { UserService } from './user.service';
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 
 @Module({
-  imports: [TokenModule],
+  imports: [forwardRef(() => TokenModule)],
   controllers: [UsersController],
   providers: [UserService],
   exports: [UserService],
